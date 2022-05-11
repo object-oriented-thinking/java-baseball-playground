17104e1732d88de0b6dbca33f5ad2c6429af6fd2
oliviarla <imoliviarla@gmail.com>
2022-05-11 13:48:28 +0900

    step0, step1 완료

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void request1(){
+        //Set의 size() 메소드를 활용해 Set의 크기를 확인
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3}) //number 파라미터로 배열의 요소 하나씩 보내 테스트
+    void request2(int number){
+        //Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void request3(String input, String excepted){
+        assertThat(numbers.contains(Integer.valueOf(input))).isEqualTo(Boolean.valueOf(excepted));
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,35 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void request1(){
+        String[] str1 = "1,2".split(",");
+        assertThat(str1).contains("1", "2");
+        String[] str2 = "1".split(",");
+        assertThat(str2).containsExactly("1");
+    }
+
+    @Test
+    void request2(){
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("test StringIndexOutOfBoundsException")
+    void request3(){
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> {"abs".charAt(4);}).withMessageMatching("String index out of range: 4");
     }
 }
