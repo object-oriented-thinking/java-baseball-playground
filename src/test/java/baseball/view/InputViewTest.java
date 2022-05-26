80247634b92dd884743ca4f51a66104b56679bfa
oliviarla <imoliviarla@gmail.com>
2022-05-26 16:23:23 +0900

    test(baseball): InputView test

---
+++ src/test/java/baseball/view/InputViewTest.java
@@ -0,0 +1,44 @@
+package baseball.view;
+
+import baseball.domain.Ball;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest {
+    InputView inputView;
+    Ball ball;
+
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView();
+        String input = "384";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+
+    @Test
+    void countTest() {
+        ball = inputView.inputBall();
+        assertThat(ball.getBalls().size()).isEqualTo(3);
+    }
+
+    @Test
+    void rangeTest() {
+        ball = inputView.inputBall();
+        assertThat(ball.getBalls().stream().filter(b -> b < 10 && b > 0).count()).isEqualTo(3);
+    }
+
+    @Test
+    void resumeTest() {
+        String data = "1";
+        InputStream in = new ByteArrayInputStream(data.getBytes());
+        System.setIn(in);
+        assertThat(inputView.resumeGame()).isTrue();
+    }
+}
