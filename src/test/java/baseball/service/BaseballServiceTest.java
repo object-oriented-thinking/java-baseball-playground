5b260ec4d7cb99e3352801024c6017009356bfc5
oliviarla <imoliviarla@gmail.com>
2022-05-27 22:27:35 +0900

    test(baseball): BaseballService 테스트 구현

---
+++ src/test/java/baseball/service/BaseballServiceTest.java
@@ -0,0 +1,48 @@
+package baseball.service;
+
+import baseball.domain.Baseball;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseballServiceTest {
+    BaseballService baseballService;
+
+    @BeforeEach
+    void setUp(){
+        baseballService= new BaseballService();
+    }
+
+    @Test
+    void isStrikeTest(){
+        Baseball userBall = new Baseball(Arrays.asList(1,2,3));
+        Baseball randomBall = new Baseball(Arrays.asList(1,2,3));
+        for(int i=0;i<3;i++){
+            assertThat(baseballService.isStrike(userBall.getBaseballs().get(i), randomBall.getBaseballs().get(i))).isTrue();
+        }
+    }
+
+    @Test
+    void isBallTest(){
+        Baseball userBall = new Baseball(Arrays.asList(1,3,2));
+        Baseball randomBall = new Baseball(Arrays.asList(2,1,3));
+        for(int i=0;i<3;i++){
+            assertThat(baseballService.isBall(userBall.getBaseballs().get(i), i, randomBall)).isTrue();
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,2,6,1,9,2,1,1", "3,7,5,7,3,9,2,0", "1,2,3,4,5,6,0,0", "3,4,8,3,4,2,0,2"})
+    void compareTest(int u1,int u2,int u3,int r1,int r2,int r3, int e1, int e2){
+        Baseball userBall = new Baseball(Arrays.asList(u1, u2, u3));
+        Baseball randomBall = new Baseball(Arrays.asList(r1, r2, r3));
+
+        assertThat(new int[]{baseballService.compare(userBall, randomBall).getBall(), baseballService.compare(userBall, randomBall).getStrike()}).isEqualTo(new int[]{e1, e2});
+        //assertThat().isEqualTo(ballStatus.getStrike());
+    }
+}
